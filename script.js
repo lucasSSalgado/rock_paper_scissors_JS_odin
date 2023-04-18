bf38f2dd92e5c9eeb46e4412ac3aa36fd88d2006
lucasSSalgado <lucssslucsss@gmail.com>
2023-04-18 12:22:19 -0300

    apply the 5 rounds per game

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function computer_play() {
+function computerSelection()  {
     let ramd = Math.floor(Math.random() * 3);
     return ramd;
 }
 
-function get_players_option() {
+function playerSelection() {
     let option = 15;
 
     do {
@@ -13,24 +13,61 @@ function get_players_option() {
     return option;
 }
 
-function print_winner() {
-    let computer = computer_play();
-    console.log(`Computer choose ${computer}!`, computer);
-    let user = get_players_option();
-    console.log(`User choose ${user}!`, user);
+function print_winner() {    
+    let user = playerSelection();
+    console.log(`User choose ${user}!`);
+    let computer = computerSelection();
+    console.log(`Computer choose ${computer}!`);
 
     if (computer == user)
     {
         console.log('Tie!');
+        return 10;
     }
     else if ((user == 0 && computer == 2) || (user == 1 && computer == 0) || (user == 2 && computer == 1))
     {
         console.log('User win!');
+        return 1;
     }
     else
     {
         console.log('Computer win!');
+        return 0;
     }
 }
 
-print_winner();
+function game() {
+    let counter = 0;
+    let players_points = 0;
+    let computers_points = 0;
+
+    while (counter < 5) 
+    {
+        let int = print_winner();
+        if (int == 1)
+        {
+            players_points++;
+        }
+        else if (int == 0)
+        {
+            computers_points++;
+        }
+
+        counter++;
+    }
+
+    if (players_points > computers_points)
+    {
+        console.log(`Players Wins!! playes: ${players_points}; computer ${computers_points}`);
+    }
+    else if (players_points < computers_points)
+    {
+        console.log(`Computer Wins!! playes: ${computers_points}; computer ${players_points}`);
+    }
+    else 
+    {
+        console.log(`Tie! Each playes do ${computers_points} points`);
+    }    
+}
+
+game();
