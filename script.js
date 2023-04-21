eab91a1a546aab5517834633fe66bc91ab0024b7
lucasSSalgado <lucssslucsss@gmail.com>
2023-04-21 17:38:54 -0300

    I function but ugly game

--- script.js
+++ script.js
@@ -6,9 +6,39 @@ let paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
     let playNumber = 0;
     let computer = computerChoose();
-    let result = winner(playNumber, computer);  
-    alert(`${result} wins`);  
+    let result = winner(playNumber, computer); 
+    
     counterPointer(result);
+    end_game(counter);
+    alert(`${result} wins`);
+    playerSocreLoad();  
+    computerSocreLoad();
+});
+
+let rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    let playNumber = 1;
+    let computer = computerChoose();
+    let result = winner(playNumber, computer);  
+
+    counterPointer(result);
+    end_game(counter);
+    alert(`${result} wins`);
+    playerSocreLoad(); 
+    computerSocreLoad(); 
+});
+
+let scissor = document.querySelector('#scissor');
+scissor.addEventListener('click', () => {
+    let playNumber = 2;
+    let computer = computerChoose();
+    let result = winner(playNumber, computer);  
+    
+    counterPointer(result);
+    end_game(counter);
+    alert(`${result} wins`);
+    playerSocreLoad();  
+    computerSocreLoad();
 });
 
 function computerChoose() {
@@ -40,4 +70,27 @@ function counterPointer(result) {
     else if (result === 'computer') {
         computer++;
     } 
+}
+function playerSocreLoad() {
+    let player_score_update = document.querySelector('#player_score_update');
+    player_score_update.textContent = player;
+}
+function computerSocreLoad() {
+    let computer_score_update = document.querySelector('#pc_score_update');
+    computer_score_update.textContent = computer;
+}
+function end_game(counter) {
+    if (counter == 5) {
+        window.open('winner.html');
+        alert('Game Over');
+        if (player > computer) {
+            alert('Player wins');
+        }
+        else if (player < computer) {
+            alert('Computer wins');
+        }
+        else {
+            alert('Draw');
+        }
+    }
 }
